14b64dbb7c932eee7e66cf07352ef987c79e4d28
Fx860 <gustafsson860@live.com>
2022-03-12 16:57:03 +0100

    alla  bitar på plats

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ const grid = document.querySelector('.grid')
 const blockWidth = 100
 const blockHight = 20
 
+const userStart = [230, 10]
+let currentPositon = userStart
+
 //crate Block 
 class Block {
     constructor(xAxis, yAxis) {
@@ -11,22 +14,40 @@ class Block {
         this.topRight = [xAxis + blockWidth, yAxis + blockHight]
     }
 }
-
 //all my blocks
 const blocks = [
-    new Block(10,270)
+    new Block(10,270),
+    new Block(120,270),
+    new Block(230,270),
+    new Block(340,270),
+    new Block(450,270),
+    new Block(10,240),
+    new Block(120,240),
+    new Block(230,240),
+    new Block(340,240),
+    new Block(450,240),
+    new Block(10,210),
+    new Block(120,210),
+    new Block(230,210),
+    new Block(340,210),
+    new Block(450,210), 
 ]
-
-
 //draw  all my blocks
 function addBlocks() {
    for (let i = 0; i <blocks.length; i++) {
    const block = document.createElement('div')
   block.classList.add('block')
-  block.style.left = blocks[i].bottomLeft[0]
-   block.style.bottom = '50px'
+  block.style.left = blocks[i].bottomLeft[0] + 'px'
+   block.style.bottom = blocks[i].bottomLeft[1] + 'px'
    grid.appendChild(block)    
    }
  }
-
  addBlocks()
+
+// add user
+const user = document.createElement('div')
+user.classList.add('user')
+user.style.left = currentPositon[0] + 'px'
+user.style.bottom = currentPositon[1] + 'px'
+grid.appendChild(user)
+
